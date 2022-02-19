2697859ce1030e43e878e27c79981ea9fadadef3
shushinasabm <shushinasabm@gmail.com>
2022-02-19 21:08:19 +0330

    creait index about us contact us

--- post/urls.py
+++ post/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import test , post
+from .views import detail, posts, test ,index, aboutus ,contactus
+app_name ="post"
  
 #post path
 urlpatterns = [
     path('test/',test, name='test' ),
     #هر چیزی که اضافه میکنیم تابع مورد نظر برگردون
-    path('post/', post, name= 'post')
+    path('posts/', posts, name= 'post'),
+    path('detail/<slug:slug>',detail, name = 'detail'),
+    path('index/',index , name ="main"),
+    path('aboutus/',aboutus , name ="aboutus"),
+    path('contactus/',contactus , name ="contactus"),
 
-
-    
-]
+    ]   
--- post/views.py
+++ post/views.py
@@ -1,20 +1,32 @@
-
-
-from django.shortcuts import render
+from multiprocessing import context
+from telnetlib import STATUS
+from django.shortcuts import render , get_object_or_404
 from django.http import  HttpResponse
 from .models import Post
 # Create your views here.
+def index (request):
+    return render(request ,'post/index.html')
+def aboutus (request):
+    return render(request ,'post/aboutus.html')
+def contactus (request):
+    return render(request ,'post/contactus.html')
+    
 def test(request):
     #دستور برگشت هر استیرینگی
     return HttpResponse("hello world")
-def post(requst):
+def posts(requst):
     #دستور برگشت خواندن همه اطلاعات از دل پست
-    posts =Post.objects.all()
+    posts =Post.objects.filter(status = 'p').order_by("-publish_at")
     # print(posts)   
     context = {
         "posts":posts
     }
     #دستورات رو از مدل گرفتیم و میخوایم به تملیت بدیم
     return render(requst, 'post/post.html', context) 
+def detail(request ,slug ):
+     context ={
+         'post':get_object_or_404(Post,slug = slug , status='p')
+     }
+     return render(request , "post/detail.html", context)
 
        
